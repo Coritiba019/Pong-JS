d567d8eb2dcfef36d0a8049c7d32ff025b67d110
Fabio Dias <fabiodias11@usp.br>
2022-11-22 10:35:35 -0300

    Initial commit

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
 //variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 13;
-let raio = diametro / 2;
+let diametro = 15;
+let raio = diametro/2;
 
 //velocidade da bolinha
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
-let raqueteComprimento = 10;
-let raqueteAltura = 90;
 
 //variáveis da raquete
 let xRaquete = 5;
 let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
 
 //variáveis do oponente
 let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 let velocidadeYOponente;
-let chanceDeErrar = 0;
 
 let colidiu = false;
+let chanceDeErrar = 0;
 
 //placar do jogo
 let meusPontos = 0;
@@ -34,7 +34,7 @@ let trilha;
 function preload(){
   trilha = loadSound("trilha.mp3");
   ponto = loadSound("ponto.mp3");
-  raquetada = loadSound("raquetada.mp3")
+  raquetada = loadSound("raquetada.mp3");
 }
 
 function setup() {
@@ -68,22 +68,24 @@ function movimentaBolinha(){
 }
 
 function verificaColisaoBorda(){
-  if(xBolinha + raio > width || xBolinha - raio < 0) {
+  if(xBolinha + raio > width ||
+    xBolinha - raio < 0){
     velocidadeXBolinha *= -1;
   }
-  
-  if(yBolinha + raio > height || yBolinha - raio < 0) {
-     velocidadeYBolinha *= -1;
+  if(yBolinha + raio > height ||
+    yBolinha - raio < 0){
+    velocidadeYBolinha *= -1;
   }
 }
 
 function mostraRaquete(x, y){
-  rect(x, y, raqueteComprimento, raqueteAltura);
+  rect(x, y, raqueteComprimento,
+    raqueteAltura);
 }
 
 function movimentaMinhaRaquete(){
   if(keyIsDown(UP_ARROW)){
-    yRaquete -= 10;    
+    yRaquete -= 10;
   }
   if(keyIsDown(DOWN_ARROW)){
     yRaquete += 10;
@@ -99,7 +101,6 @@ function verificaColisaoRaquete(){
 
 function verificaColisaoRaquete(x, y){
   colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
-  
   if(colidiu){
     velocidadeXBolinha *= -1;
     raquetada.play();
@@ -109,18 +110,18 @@ function verificaColisaoRaquete(x, y){
 function movimentaRaqueteOponente(){
   velocidadeYOponente = yBolinha -yRaqueteOponente - raqueteComprimento / 2 - 30;
   yRaqueteOponente += velocidadeYOponente + chanceDeErrar
-  calculaChanceDeErrar();
+  calculaChanceDeErrar()
 }
 
 function incluiPlacar(){
   stroke(255);
   textAlign(CENTER);
   textSize(16);
-  fill(color(255, 140, 0));
+  fill(color(148,0,211));
   rect(150, 10, 40, 20);
   fill(255);
   text(meusPontos, 170, 26);
-  fill(color(255, 140, 0));
+  fill(color(148,0,211));
   rect(450, 10, 40, 20);
   fill(255);
   text(pontosDoOponente, 470, 26);
